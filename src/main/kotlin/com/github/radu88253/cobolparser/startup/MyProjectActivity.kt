e8f897b09e20edc65eaf7c06319b244efa869285
GitHub Action <action@github.com>
2025-12-25 13:56:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/radu88253/cobolparser/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/radu88253/cobolparser/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.radu88253.cobolparser.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/radu88253/cobolparser/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/radu88253/cobolparser/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.radu88253.cobolparser.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.radu88253.cobolparser.MyBundle
+import com.github.radu88253.cobolparser.services.MyProjectService
 import javax.swing.JButton
 
 
